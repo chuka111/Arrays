8587938c0e0d98ea413319172255a18cbb912f6f
Chuka <g00414637@atu.ie>
2023-11-06 09:45:23 +0000

    added an array called anArray that prints out 10 values

--- src/main/java/ie/atu/Arrays.java
+++ src/main/java/ie/atu/Arrays.java
@@ -1,4 +1,32 @@
 package ie.atu;
 
 public class Arrays {
+
+
+    public static void main(String[] args) {
+
+        int [] anArray;
+
+        anArray = new int[10];
+
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        for (int i = 0; i <= 9; i++) {
+
+
+            System.out.println("Element at index " + i + " : " + anArray[i]);
+        }
+
+
+
+    }
 }
